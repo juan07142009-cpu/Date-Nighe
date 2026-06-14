3113c1258abd8d73f86614ab372d847f697a186c
juan07142009-cpu <juan07142009@gmail.com>
2026-06-14 01:59:14 -0400

    Delete src/routes/mian.tsx

--- src/routes/mian.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/main')({
-  component: Main,
-})
-
-function Main() {
-  return <div>Main Page</div>
-}
